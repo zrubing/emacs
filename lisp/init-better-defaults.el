ae0622a2c5d102ccb5a1943960f9791470d9c717
rubing <rubingem@163.com>
2017-12-02 17:09:00 +0800

    Highlight enclosing parens

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -43,4 +43,10 @@
                                         try-complete-lisp-symbol-partially
                                         try-complete-lisp-symbol))
 
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight enclosing parens."
+  (cond ((looking-at-p "\\s(") (funcall fn))
+        (t (save-excursion
+             (ignore-errors (backward-up-list))
+             (funcall fn)))))
 (provide 'init-better-defaults)
