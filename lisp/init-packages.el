c474d02c7c600dfe5283dcaf9a7d37ff3ea40beb
rubing <rubingem@163.com>
2017-12-02 17:28:52 +0800

    add ctrl n ctrl-p to choose company

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -57,6 +57,12 @@
 
 (global-company-mode 1)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (require 'popwin)
 (popwin-mode 1)
 
