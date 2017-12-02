3932c21aa8e7d8a86aacf5170549d8d680f32a80
rubing <rubingem@163.com>
2017-12-02 16:55:56 +0800

    formate packages.el

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,51 +1,51 @@
 (when (>= emacs-major-version 24)
-     (require 'package)
-     (package-initialize)
-     (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-                      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
 
 
- ;; cl - Common Lisp Extension
- (require 'cl)
+;; cl - Common Lisp Extension
+(require 'cl)
 
- ;; Add Packages
- (defvar my/packages '(
-                ;; --- Auto-completion ---
-                company
-                ;; --- Better Editor ---
-                hungry-delete
-                swiper
-                popwin
-                counsel
-		;;org-mode
-                smartparens
-                ;; --- Major Mode ---
-                js2-mode
-                ;; --- Minor Mode ---
-                nodejs-repl
-                exec-path-from-shell
-                ;; --- Themes ---
-                monokai-theme
-                ;; solarized-theme
-                ) "Default packages")
+;; Add Packages
+(defvar my/packages '(
+		      ;; --- Auto-completion ---
+		      company
+		      ;; --- Better Editor ---
+		      hungry-delete
+		      swiper
+		      popwin
+		      counsel
+		      ;;org-mode
+		      smartparens
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ;; --- Themes ---
+		      monokai-theme
+		      ;; solarized-theme
+		      ) "Default packages")
 
- (setq package-selected-packages my/packages)
+(setq package-selected-packages my/packages)
 
 (defun my/packages-installed-p ()
-     (loop for pkg in my/packages
-           when (not (package-installed-p pkg)) do (return nil)
-           finally (return t)))
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
 
- (unless (my/packages-installed-p)
-     (message "%s" "Refreshing package database...")
-     (package-refresh-contents)
-     (dolist (pkg my/packages)
-       (when (not (package-installed-p pkg))
-         (package-install pkg))))
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
 
- ;; Find Executable Path on OS X
- (when (memq window-system '(mac ns))
-   (exec-path-from-shell-initialize))
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 ;;js2-mode
 (setq auto-mode-alist
